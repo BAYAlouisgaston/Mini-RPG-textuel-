ff6f2eb1f7df69ed0765412f87f687f7c29031d1
BAYA Louis Gaston <bayalouisgaston7@gmail.com>
2026-05-14 19:54:07 +0200

    changement de dialogues

--- RPG.py
+++ RPG.py
@@ -12,7 +12,7 @@ def lance_combat(potions_joueur):
         if action == "attaquer":
             degats =random.randint(10,30)
             pv_m -= degats
-            print(f"Tu as infliger  un  coup!! -{degats} PV pour le loup!!.")
+            print(f"Tu as infligé  un  coup!! -{degats} PV pour le loup!!.")
         elif action == "soigner":
             if potions_joueur > 0:
                 soin = random.randint(25, 40)
@@ -20,7 +20,7 @@ def lance_combat(potions_joueur):
                 potions_joueur -= 1
                 print(f"Potion bue ! +{soin} PV. Il te reste {potions_joueur} potions.")
             else:
-                print("tu n'a plus de  potions !")
+                print("tu n'as plus de  potions !")
         # Tour du monstre
         if pv_m > 0:
             degats_m = random.randint(10, 30)
@@ -36,7 +36,7 @@ def lance_combat(potions_joueur):
 #  FONCTION PRINCIPALE (EXPLORATION)
 def jeu():
     stock_potions = 1 # Le joueur commence avec une seule potion
-    print("Bienvenue dans l'aventure Python !")
+    print("Bienvenue dans cette mini aventure !")
    
     #  Le Village
     print("\nTu es au village. Un vieux sage te donne une potion gratuite.")
@@ -54,6 +54,6 @@ def jeu():
             print(f"\nTu sors de la grotte avec {stock_potions} potions en poche.")
             print("Félicitations, tu as terminé cette démo !")
     else:
-        print("\nTu restes au village tranquillement et le vieil homme vous laisse une étrange épée. Fin de cette démo.")
+        print("\nTu restes au village tranquillement et le vieil homme te laisse une étrange épée. Fin de cette démo.")
 # --- LANCEMENT ---
 jeu()
